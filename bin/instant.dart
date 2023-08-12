9ebf3e2a12a5ab26f1cd0fc7595ea738fd3ae0ef
Mohammed <109498283+MohammedEmadSelim@users.noreply.github.com>
2023-08-12 13:07:28 +0300

    ashraf

--- bin/instant.dart
+++ bin/instant.dart
@@ -4,34 +4,7 @@ import 'dart:math';
 import 'package:instant/instant.dart' as instant;
 
 void main() {
-  print('please, enter years of study');
-  final int studyYears = int.parse(stdin.readLineSync()!);
-  final double numberOfSemesters = studyYears * 2;
-  double gpa;
-  double cgpa = 0;
-  // this loop for scaping numbr above 4 and number 0 and make repeative sum 
-  for (int i = 0; i < numberOfSemesters; i++) {
-    print('please, enter the gpa for semster ${i + 1}');
-    gpa = double.parse(stdin.readLineSync()!);
-    if (gpa == 0 || gpa > 4) {
-      print('error gpa,try again');
-      i--;
-    }
-    if (gpa > 0 && gpa <= 4){
-      cgpa += gpa;
-    }
-  }
-  print(cgpa / numberOfSemesters );
-  String degree=switch(cgpa / numberOfSemesters){
-     <= 4 && >=3.5=> 'A',
-     <= 3.4 && >=3=> 'B',
-     <= 2.9 && >=2.5=> 'C',
-     <= 2.4 && >=2=> 'D',
-    _=> 'F'
-  };
-  print('your degree is $degree');
-}
-  /* task last seasion
+//task one 
   print('enter ypur number');
   final int num = int.parse(stdin.readLineSync()!);
   int checkPrime = 0 ;
@@ -65,82 +38,40 @@ void main() {
   }
   
   print('\naverge = ${num/2}');
-  */
+  
 
-//   // int? x;
-//   // int z = 5;
-//   // if (z == 5) {
-//   //   x = 5;
-//   // }
-//   // if (x !=null) {
-//   //   int y = 5 + x;
-//   //   print(y);
-//   // }
 
-//   // x = z != null ? z : 8;
-//   // x = z ?? 8;
-//   // x ??=5;
 
-// /*
-// abd elrahman
-// //   int exp =2;
-// //   double bonus;
-// // print ("enter your track");
-// // String track= stdin.readLineSync()!;
-// // print('enter your salary');
-// // double Salary =double.parse(stdin.readLineSync()!);
-// // track = 'flutter';
-// // switch (track) {
-// //   case 'FlutterTrack' when exp <= 2:
-// //   bonus = .1;
-// //   Salary += Salary * bonus;
-// //   print('bonus is ${bonus * 100}');
-// //   print('your salary is $Salary');
-// }
-// */
+//==============================================================================================
+//task two
 
-//   // mohammed emad selim
-//   // print('please, enter your track');
-//   // String track = stdin.readLineSync()!;
-//   // track = (track.toLowerCase()).toString();
-//   // print('please, enter yoor salary');
-//   // double salary = double.parse(stdin.readLineSync()!);
-//   // print('please, enter yoor experince');
-//   // final int exp = int.parse(stdin.readLineSync()!);
-//   // final double bouns;
-//   // bouns = switch (track) {
-//   //   'flutter' when exp <= 2 => 0.10,
-//   //   'back end ' when exp <= 2 => .15,
-//   //   'flutter' when exp <= 4 => .20,
-//   //   'back end ' when exp <= 4 => .25,
-//   //   'flutter' when exp > 4 => .30,
-//   //   'back end ' when exp >4 => .30,
-//   //   _ => 0,
-//   // };
-//   // print(bouns);
 
-//   // salary += salary*bouns;
-//   // print(salary);
-//   // print('your salary after bouns is $salary');
 
-//   // const double bi = 3.14;
-//   // print('enter the value of circle reduis');
-//   // final double circleReduis = double.parse(stdin.readLineSync()!);
-//   // final circleArea = (circleReduis * circleReduis) * bi;
-//   // print("circle area = $circleArea cm");
-
-//   // print('please, enter your bouns');
-//   // final double bouns = double.parse(stdin.readLineSync()!);
-//   // if (bouns < 50) {
-//   //   print('fail');
-//   // } else if (bouns < 60) {
-//   //   print('Accepted');
-//   // } else if (bouns < 75) {
-//   //   print('Good');
-//   // } else if (bouns < 85) {
-//   //   print('Very good');
-//   // } else if (bouns < 100) {
-//   //   print('Excellent');
-//   // }
-// }
-
+  print('please, enter years of study');
+  final int studyYears = int.parse(stdin.readLineSync()!);
+  final double numberOfSemesters = studyYears * 2;
+  double gpa;
+  double cgpa = 0;
+  // this loop for scaping numbr above 4 and number 0 and make repeative sum 
+  for (int i = 0; i < numberOfSemesters; i++) {
+    print('please, enter the gpa for semster ${i + 1}');
+    gpa = double.parse(stdin.readLineSync()!);
+    if (gpa == 0 || gpa > 4) {
+      print('error gpa,try again');
+      i--;
+    }
+    if (gpa > 0 && gpa <= 4){
+      cgpa += gpa;
+    }
+  }
+  print(cgpa / numberOfSemesters );
+  String degree=switch(cgpa / numberOfSemesters){
+     <= 4 && >=3.5=> 'A',
+     <= 3.4 && >=3=> 'B',
+     <= 2.9 && >=2.5=> 'C',
+     <= 2.4 && >=2=> 'D',
+    _=> 'F'
+  };
+  print('your degree is $degree');
+}
+  
